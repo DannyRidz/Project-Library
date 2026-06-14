e1bd0e33530f4ce1239a5d70fb752b18d91284d9
DannyRidz <danny.ridz93@gmail.com>
2026-06-14 10:24:18 +0800

    Add toggleRead method to Book prototype

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+};
+
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
